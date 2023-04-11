be3be87a27a5094608d1bba853865c8029166fa9
Sanalicyber <omerfaruk.h@outlook.com>
2023-04-12 02:07:46 +0300

    Camera fix

---
+++ Assets/Scripts/CustomButton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace StarterAssets
+{
+    public class CustomButton : MonoBehaviour
+    {
+        [Serializable]
+        public class RaycastEnterEvent : UnityEngine.Events.UnityEvent { }
+        [Serializable]
+        public class RaycastExitEvent : UnityEngine.Events.UnityEvent { }
+        
+        public RaycastEnterEvent raycastEnterEvent;
+        public RaycastExitEvent raycastExitEvent;
+
+        public void OnMouseDown ()
+        {
+            Debug.Log("OnMouseDown");
+            raycastEnterEvent.Invoke();
+        }
+    }
+}
